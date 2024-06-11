97a8853cc975dc77733cbe7d6853254078163e51
BestRivenna <1043744584@qq.com>
2024-06-11 15:05:05 +0800

    auto commit Docs DevCode.

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -20,10 +20,9 @@ cd ./docs
 npm run build
 
 cd src/dist/
-git init
 git add .
-git commit -m "auto commit project"
-git pull --allow-unrelated-histories ${PRO} master
+git commit -m "auto commit docs/dist"
+#git pull --allow-unrelated-histories ${PRO} master
 git push -f ${PRO} master
 if [ $? -eq 0 ]; then
 	echo "上线代码提交完成"
